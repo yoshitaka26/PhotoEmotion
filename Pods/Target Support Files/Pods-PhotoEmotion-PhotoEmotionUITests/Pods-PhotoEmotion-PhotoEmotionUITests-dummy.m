ec42102bb7e69b31921279d5a2975e94770d0a38
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2022-05-24 17:28:30 +0900

    add gitignore

--- Pods/Target Support Files/Pods-PhotoEmotion-PhotoEmotionUITests/Pods-PhotoEmotion-PhotoEmotionUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoEmotion_PhotoEmotionUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoEmotion_PhotoEmotionUITests
-@end
--- Pods/Target Support Files/Pods-PhotoEmotion/Pods-PhotoEmotion-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoEmotion : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoEmotion
-@end
